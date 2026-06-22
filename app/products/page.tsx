0de4735c7a7192ddadc09dab65bf292a8731409a
Kleyton Ferreira <kleytondev40@gmail.com>
2026-06-22 17:46:09 -0300

    feat: add initial pages

---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+
+
+const ProductPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default ProductPage
---
+++ app/sales/page.tsx
@@ -0,0 +1,5 @@
+const SalesPage = () => {
+  return <div>page</div>;
+};
+
+export default SalesPage;
