1bc3f84b42fb3a3a8b474c26e14401cbf49a3fa6
Kleyton Ferreira <kleytondev40@gmail.com>
2026-06-28 22:53:32 -0300

    refactor: toast updat and created

--- app/products/_components/upsert-dialog-dialog.tsx
+++ app/products/_components/upsert-dialog-dialog.tsx
@@ -29,6 +29,7 @@ import {
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createProduct } from "@/app/_actions/product/create-product";
+import { toast } from "sonner";
 
 interface UpsetProducDialogProps {
   defaultValues?: CreateProductSchema;
@@ -54,6 +55,10 @@ const UpsetProducDialog = ({
   const onSubmit = async (data: CreateProductSchema) => {
     try {
       await createProduct({ ...data, id: defaultValues?.id });
+
+      isEditing
+        ? toast.success("Produto atualizado com sucesso!")
+        : toast.success("Produto criado com sucesso!");
       onSuccess?.();
     } catch (error) {
       console.log(error);
