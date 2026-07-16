d19c192f80f40d3566822d245b7d03459946ffb8
Kleyton Ferreira <kleytondev40@gmail.com>
2026-07-15 22:51:16 -0300

    feat: ensure products stock is restored after sale is deleted

--- app/_actions/product/sale/delete-sale/index.ts
+++ app/_actions/product/sale/delete-sale/index.ts
@@ -6,10 +6,39 @@ import { db } from "@/app/_lib/prisma";
 import { revalidatePath } from "next/cache";
 
 export const deleteSale = actionClient.schema(deleteSaleSchema).action(async ({ parsedInput: { id } }) => {
-    await db.sale.delete({
-        where: {
-            id
+
+    await db.$transaction(async (trx) => {
+        const sale = await trx.sale.findUnique({
+            where: {
+                id,
+            },
+            include: {
+                products: true
+            }
+        })
+        if (!sale) return
+        await trx.sale.delete({
+            where: {
+                id
+            }
+        })
+
+        for (const product of sale.products) {
+            await trx.product.update({
+                where: {
+                    id: product.productId
+                },
+                data: {
+                    stock: {
+                        increment: product.quantity
+                    }
+                }
+            })
         }
+
     })
+
     revalidatePath("/sale")
+    revalidatePath("/products")
+    revalidatePath("/")
 })
